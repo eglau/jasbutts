4eaa4393077d5f73eaf30470a08a95cf7431bffa
Edward Lau <edwardglau@gmail.com>
2019-11-21 22:40:24 -0800

    Added title prop for link

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -6,17 +6,24 @@ class CustomLink extends React.Component {
   static get propTypes() {
     return {
       to: PropTypes.string.isRequired,
+      title: PropTypes.string
     };
   }
 
   render() {
+    // Try to set title if not given
+    let title = this.props.title;
+    if (!title && typeof this.props.children === 'string') {
+      title = this.props.children;
+    }
+
     // External link
     if (/^https?:\/\//.test(this.props.to)) {
-      return <a href={this.props.to} target="_blank" rel="noopener noreferrer">{this.props.children}</a>
+      return <a href={this.props.to} target="_blank" rel="noopener noreferrer" title={title}>{this.props.children}</a>
     }
 
     return (
-      <Link to={this.props.to}>{this.props.children}</Link>
+      <Link to={this.props.to} title={title}>{this.props.children}</Link>
     );
   }
 }
